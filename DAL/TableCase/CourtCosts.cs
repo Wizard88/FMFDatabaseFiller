6d9d9a03558b3dd33ef97713b4c31f156fc0a589
milan milicevic <m.milicevic.etf@hotmail.com>
2020-12-02 21:11:56 +0100

    s

--- DAL/TableCase/CourtCosts.cs
+++ DAL/TableCase/CourtCosts.cs
@@ -20,29 +20,29 @@ namespace DAL.TableCase
                 object vrstaSudTroskova = row["VrstaSudTroskova"];
                 object datumUnosa = row["DatumUnosa"];
 
-                SqlCommand cmdIzvr = new SqlCommand("BudgetYearInsert", SQLSingleton.Instance.SqlConnection)
+                SqlCommand cmdIzvr = new SqlCommand("", SQLSingleton.Instance.SqlConnection)
                 {
                     CommandType = System.Data.CommandType.StoredProcedure,
                     Transaction = transaction
                 };
 
-                cmdIzvr.Parameters.AddWithValue("@BudgetYear", budzetskaGodina);
+                cmdIzvr.Parameters.AddWithValue("",);
 
-                SqlCommand cmdPar = new SqlCommand("BudgetYearInsert", SQLSingleton.Instance.SqlConnection)
+                SqlCommand cmdPar = new SqlCommand("", SQLSingleton.Instance.SqlConnection)
                 {
                     CommandType = System.Data.CommandType.StoredProcedure,
                     Transaction = transaction
                 };
 
-                cmdPar.Parameters.AddWithValue("@BudgetYear", budzetskaGodina);
+                cmdPar.Parameters.AddWithValue("@", );
 
-                SqlCommand cmdTotal = new SqlCommand("BudgetYearInsert", SQLSingleton.Instance.SqlConnection)
+                SqlCommand cmdTotal = new SqlCommand("", SQLSingleton.Instance.SqlConnection)
                 {
                     CommandType = System.Data.CommandType.StoredProcedure,
                     Transaction = transaction
                 };
 
-                cmdTotal.Parameters.AddWithValue("@BudgetYear", budzetskaGodina);
+                cmdTotal.Parameters.AddWithValue("@", budzetskaGodina);
 
 
                 cmdIzvr.ExecuteNonQuery();
